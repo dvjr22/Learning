2676a85bc4980accbc97bc586fbc23cc4aac57f3
valdeslab <dvjr22@gmail.com>
2018-01-12 10:51:10 -0500

    Finished book

--- LearningR/ch_8_data.R
+++ LearningR/ch_8_data.R
@@ -111,7 +111,129 @@ dotplot(factor(Month) ~ SAL | Station, pch = 16,
 
 #8.6.3
 
+Sparrows<-read.table(file="RBook/Sparrows.txt", header=TRUE)
+names(Sparrows)
 
+#[1] "Species"  "Sex"      "Wingcrd"  "Tarsus"   "Head"     "Culmen"
+#[7] "Nalospi"  "Wt"       "Observer" "Age"
+
+library(lattice)
+
+
+xyplot(Wingcrd ~ Tarsus | Species * Sex,
+       xlab = "Axis 1", ylab = "Axis 2", data = Sparrows,
+       xlim = c(-1.1, 1.1), ylim = c(-1.1, 1.1),
+       panel = function(subscripts, ...){
+         zi <- Sparrows[subscripts, 3:8]
+         di <- princomp(zi, cor = TRUE)
+         Load <- di$loadings[, 1:2]
+         Scor <- di$scores[, 1:2]
+         panel.abline(a = 0, b = 0, lty = 2, col = 1)
+         panel.abline(h = 0, v = 0, lty = 2, col = 1)
+         for (i in 1:6){
+           llines(c(0, Load[i, 1]), c(0, Load[i, 2]), 
+                  col = 1, lwd = 2)
+           ltext(Load[i, 1], Load[i, 2], 
+                 rownames(Load)[i], cex = 0.7)}
+         sc.max <- max(abs(Scor))
+         Scor <- Scor / sc.max
+         panel.points(Scor[, 1], Scor[, 2], pch = 1,
+                      cex = 0.5, col = 1)
+       })
+
+
+
+S1<-Sparrows[Sparrows$Species=="SESP" & Sparrows$Sex=="Female",3:8]
+
+di <- princomp(S1, cor = TRUE)
+Load <- di$loadings[, 1:2]
+Scor <- di$scores[, 1:2]
+
+
+
+
+cloud(CHLFa ~ T * SAL | Station, data = Env,
+      screen = list(z = 105, x = -70),
+      ylab = "Sal", xlab = "T", zlab = "Chl. a",
+      ylim = c(26,33),
+      subset = (Area=="OS"),
+      scales = list(arrows = FALSE))
+
+
+
+Hawaii <- read.table("RBook/waterbirdislandseries.txt", header = TRUE)
+library(lattice)
+
+names(Hawaii)
+
+Birds <- as.vector(as.matrix(Hawaii[,2:9]))
+
+
+Time <- rep(Hawaii$Year, 8)
+MyNames <- c("Stilt_Oahu","Stilt_Maui","Stilt_Kauai_Niihau",
+             "Coot_Oahu","Coot_Maui","Coot_Kauai_Niihau",
+             "Moorhen_Oahu","Moorhen_Kauai")
+ID <- rep(MyNames, each = 48)
+
+
+xyplot(Birds ~ Time|ID, ylab = "Bird abundance",
+       layout = c(3, 3), type = "l", col = 1)
+
+
+
+ID2 <- factor(ID,
+              levels=c(
+                "Stilt_Oahu",
+                "Stilt_Kauai_Niihau",
+                "Stilt_Maui",
+                "Coot_Oahu",
+                "Coot_Kauai_Niihau",
+                "Coot_Maui",
+                "Moorhen_Oahu",
+                "Moorhen_Kauai"))
+
+
+xyplot(Birds ~ Time|ID2, ylab = "Bird abundance",
+       layout = c(3, 3), type = "l", col = 1)
+
+
+
+
+xyplot(Birds ~ Time|ID2, ylab = "Bird abundance",
+       layout = c(3, 3), type = "l", col = 1,
+       scales = list(x = list(relation = "same"),
+                     y = list(relation = "free"),
+                     tck=-1))
+
+Species <-rep(c("Stilt","Stilt","Stilt",
+                "Coot","Coot","Coot",
+                "Moorhen","Moorhen"),each = 48)
+
+xyplot(Birds ~ Time|Species, ylab = "Bird abundance",
+       layout = c(2, 2), type = "l", col = 1,
+       scales = list(x = list(relation = "same"),
+                     y = list(relation = "free")),
+       groups = ID, lwd=c(1,2,3))
+
+
+
+xyplot(Stilt_Oahu + Stilt_Maui + Stilt_Kauai_Niihau ~ Year,
+       ylab = "Bird abundance", data = Hawaii,
+       layout = c(2, 2), type = "l", col = 1,
+       scales = list(x = list(relation = "same"),
+                     y = list(relation = "free")))
+
+
+Env <- read.table(file ="RBook/RIKZENV.txt", header = TRUE)
+library(lattice)
+
+AllAreas <- levels(unique(Env$Area))
+for (i in AllAreas  ){ 
+  Env.i <- Env[Env$Area==i,]
+  dotplot(factor(Month)~SAL | Station, 
+          data = Env.i)
+  win.graph()
+}
 
 
 
